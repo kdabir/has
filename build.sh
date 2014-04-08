8065f2f80fc6818b01650a25b1d32b50840fcd66
Saager Mhatre <saager.mhatre@gmail.com>
2014-04-08 20:17:55 +0530

    Update build script; refactor groups into loop

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-ruby build.rb groovy_dev > bin/groovy_dev/dq.sh
-ruby build.rb ruby_dev > bin/ruby_dev/dq.sh
-ruby build.rb scala_dev > bin/scala_dev/dq.sh
-ruby build.rb java_dev > bin/java_dev/dq.sh
-ruby build.rb db > bin/db/dq.sh
-ruby build.rb core > bin/core/dq.sh
-ruby build.rb clojure_dev > bin/clojure_dev/dq.sh
-ruby build.rb all > bin/all/dq.sh
-ruby build.rb frontend_dev > bin/frontend_dev/dq.sh
+
+rm -rf ./bin
+
+for group in groovy_dev ruby_dev scala_dev java_dev db core clojure_dev all frontend_dev; do
+  mkdir -p ./bin/$group/
+  ruby build.rb $group > ./bin/$group/dq.sh
+done
