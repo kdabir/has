e2702935b4b029732f7cbe6a2ed6b9ee201cfb3e
Saager Mhatre <saager.mhatre@gmail.com>
2014-04-08 20:18:34 +0530

    Update build script; make artifacts executable

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ for group in groovy_dev ruby_dev scala_dev java_dev db core clojure_dev all fron
   mkdir -p ./bin/$group/
   ruby build.rb $group > ./bin/$group/dq.sh
 done
+
+find bin -type f -name \*.sh -exec chmod u+x {} +
