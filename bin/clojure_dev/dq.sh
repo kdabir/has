11984e8d8160201c3cd02d44a151fc38a5e93fbf
Saager Mhatre <saager.mhatre@gmail.com>
2014-05-12 15:44:41 +0530

    Update reporter; colourize output
    Closes #6

--- bin/clojure_dev/dq.sh
+++ bin/clojure_dev/dq.sh
@@ -1,14 +1,22 @@
 OK=0
 KO=0
 
+PASS='✔'
+FAIL='✘'
+
+if [[ $TERM == xterm-*color ]]; then
+  PASS="\E[32m$PASS\E[0m"
+  FAIL="\E[31m$FAIL\E[0m"
+fi
+
 _dq_report () {
-	if [ "$2" -eq 0 ]; then
-	 	echo "✔ $1"
-		OK=$(($OK+1))
-	else
-		echo "✘ $1"
-	    KO=$(($KO+1))
-	fi
+  if [ "$2" -eq 0 ]; then
+    echo -e "$PASS $1"
+    OK=$(($OK+1))
+  else
+    echo -e "$FAIL $1"
+    KO=$(($KO+1))
+  fi
 }
 
 ## surprisingly no option for version that i could find of
@@ -21,6 +29,6 @@ _dq_report 'java' $?
 
 lein -v > /dev/null 2>&1
 _dq_report 'leiningen' $?
-echo Your dq is $OK / $(($OK+$KO))
+echo; echo Your dq is $OK / $(($OK+$KO))
 
 exit $KO
