a93e9a51ec3adfa7ef799924e1b715ac28e1e57a
Éric <89578893+eric-glb@users.noreply.github.com>
2024-10-16 14:48:17 +0200

    clean packages_all.sh output

--- tests/packages_all.sh
+++ tests/packages_all.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 pushd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null
 
-grep -o "^ \\+[a-zA-Z0-9_|-]\\+)" ../has | grep -o "[a-zA-Z0-9_|-]\\+" | tr "|" "\\n" | sort -f
+grep -o "^ \\+[a-zA-Z0-9][a-zA-Z0-9_|-]\\+)" ../has | grep -o "[a-zA-Z0-9_|-]\\+" | tr "|" "\\n" | sort -f
 
 popd >/dev/null
